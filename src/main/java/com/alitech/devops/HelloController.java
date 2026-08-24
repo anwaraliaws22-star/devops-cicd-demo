9374de60ef3d41eba1f61e67533c3df079a103e6
Anwar Ali <anwaraliaws22@gmail.com>
2026-08-24 14:34:08 +0530

    first commit

---
+++ src/main/java/com/alitech/devops/HelloController.java
@@ -0,0 +1,23 @@
+package com.alitech.devops;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @Value("${app.version:1.0.0}")
+    private String version;
+
+    @GetMapping("/api/hello")
+    public Map<String, String> hello() {
+
+        return Map.of(
+                "message", "Hello from DevOps Demo",
+                "version", version
+        );
+    }
+}
---
+++ src/test/java/com/alitech/devops/HelloControllerTest.java
@@ -0,0 +1,12 @@
+package com.alitech.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloControllerTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
