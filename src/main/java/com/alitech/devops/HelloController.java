f8b4769fe4f2093082094377a537be1822b6388e
Anwar Ali <anwaraliaws22@gmail.com>
2026-08-28 22:11:01 +0530

    updated v7

--- src/main/java/com/alitech/devops/HelloController.java
+++ src/main/java/com/alitech/devops/HelloController.java
@@ -16,7 +16,7 @@ public class HelloController {
     public Map<String, String> hello() {
 
         return Map.of(
-                "message", "Hello from DevOps CI/CD Pipeline v6",
+                "message", "Hello from DevOps CI/CD Pipeline v7",
                 "version", version
         );
     }
